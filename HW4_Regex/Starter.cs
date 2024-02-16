b543e580dc29d5d7e1db877cc202923d33e79223
TymoshchukMN <TymoshchukMN@gmail.com>
2024-02-16 09:47:34 +0200

    1st task done

---
+++ HW4_Regex/Starter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HW4_Regex
+{
+    internal class Starter
+    {
+        public static void Run()
+        {
+            //////
+            // Користувач вводить строку. Виконайте перевірку, використовуючи регулярні вирази:
+            // Що строка введена латиницею і не містить окремих букв кирилиці або цифр;
+            // Строка введена латиницею і містить окремі літери кирилиці, не містить цифри;
+            // Строка введена латиницею і містить цифри, не містить кирилицю.
+            ///////
+
+            string userInput = string.Empty;
+            const string Exit = "exit";
+
+            // 1. о строка введена латиницею і не містить окремих букв кирилиці або цифр;
+            const string Mask1 = @"^[a-zA-Z]+$";
+            Regex regex = new Regex(Mask1);
+            do
+            {
+                UI.PrintRequest();
+                userInput = Console.ReadLine();
+
+                if (regex.Match(userInput).Success)
+                {
+                    UI.PrintRes("Условия выполнены.\n", ConsoleColor.Green);
+                }
+                else
+                {
+                    UI.PrintRes("Условия НЕ выполнены.\n", ConsoleColor.Red);
+                }
+            }
+            while (userInput != Exit);
+        }
+    }
+}
---
+++ HW4_Regex/UI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HW4_Regex
+{
+    internal class UI
+    {
+        public static void PrintRes(string str, ConsoleColor color)
+        {
+            ConsoleColor def = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(str);
+            Console.ForegroundColor = def;
+        }
+
+        public static void PrintRequest()
+        {
+            Console.Write("Введеите строку для проверки:\t");
+        }
+    }
+}
